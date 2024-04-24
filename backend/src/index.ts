a46e616527656d3bedce939c3f7058e593a97c8c
Alexander Braun <alexanderbraun@freenet.de>
2024-04-25 00:30:19 +0200

    fix: serve frontend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,12 +1,13 @@
-import { Express, Request, Response } from "express";
+import express, { Express, Request, Response } from "express";
+import path from "path";
 import app from "./app";
-const port = process.env.PORT;
-const express = require("express");
-const path = require("path");
+
+const port = process.env.PORT || 3000; // Default to 3000 if PORT is not set
 
 app.use(express.static(path.join(__dirname, "../frontend/dist/frontend")));
+
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Origin", "*"); // Update to match the domain you will make the request from if needed
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept, Authorization"
@@ -19,9 +20,13 @@ app.use(function (req, res, next) {
 });
 
 app.listen(port, () => {
-  console.log("Server is up on port", port);
+  console.log(`Server is up on port ${port}`);
 });
 
-app.get("", (req, res) => {
+app.get("/", (req, res) => {
   res.send({ hello: "HELLO!" });
+});
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../frontend/dist/frontend/index.html"));
 });
