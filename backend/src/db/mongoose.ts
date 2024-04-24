8637ff1a0d43aeb59666f4d907236a1e44b30694
Alexander Braun <alexanderbraun@freenet.de>
2024-04-25 00:38:03 +0200

    fix: install dotenv

--- backend/src/db/mongoose.ts
+++ backend/src/db/mongoose.ts
@@ -1,11 +1,20 @@
 const mongoose = require("mongoose");
 const { of } = require("rxjs");
+const dotenv = require("dotenv");
+dotenv.config();
 
 console.log(process.env.MONGO_DB_URL, process.env.DATABASE_NAME);
-//mongoose.set("debug", process.env.ENV === "DEVELOPMENT");
+const mongoURI = process.env.MONGO_DB_URL;
+if (!mongoURI) {
+  console.error("MongoDB URI is not set.");
+  process.exit(1);
+}
+mongoose.set("debug", process.env.ENV === "DEVELOPMENT");
 of(
-  mongoose.connect(process.env.MONGO_DB_URL, {
+  mongoose.connect(mongoURI, {
     dbName: process.env.DATABASE_NAME,
     autoIndex: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
   })
 ).subscribe(() => console.log("MONGOOSE CONNECTED"));
